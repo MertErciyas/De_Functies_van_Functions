a55ed0ceed5ec9d81574c2f22fd5cd5d74cfbc3b
Mert Erciyas <99058235@mydavinci.nl>
2021-12-21 10:26:24 +0100

    Game Over

--- Opdrachten/Game_Over/The_Game_Over.py
+++ Opdrachten/Game_Over/The_Game_Over.py
@@ -42,28 +42,39 @@ def itemPickup(item,listNum):
   print(".\n")
   invList[listNum] = item
 
+#lists for the 3 items player needs to pick up
+invList = ["item0","item1","item2"]
+kitchenList = ["·dog food", "·a knife", "·a spoon"]
+bathList = ["·soap", "·a towel", "·some toiletpaper"]
+bedList = ["·your pillow", "·your bed sheets", "·your sleeping girlfriend"]
+
+
 #title screen
-print(" \
-                                                              _____^_       \n  \
-                                                            |    |  \       \n  \
-                                                            \   /  ^ |      \n  \
-                                                           / \_/   0  \     \n  \
-                                                          /            \    \n  \
-                                                         /    ____      0   \n  \
-                                                        /      /  \___ _/   \n  \
-             __                __        __    __                      __                          __                       __      \n  \
-           /\ \              /\ \__    /\ \__/\ \                    /\ \                        /\ \          __         /\ \      \n  \
- __  __  __\ \ \___      __  \ \ ,_\   \ \ ,_\ \ \___      __        \_\ \    ___      __        \_\ \    ___ /\_\    ___ \ \/      \n  \
-/\ \/\ \/\ |\ \  _ `\  /'__`\ \ \ \/    \ \ \/\ \  _ `\  /'__`\      /'_` \  / __`\  /'_ `\      /'_` \  / __`\/\ \ /' _ `\|/       \n  \
-\ \ \_/ \_/| \ \ \ \/\ \L\.\_\ \ \_      \ \ \_\ \ \ \ \/\  __/     /\ \L\ \/\ \L\ \/\ \L\ \    /\ \L\ \/\ \L\ \ \ \/\ \/\ \        \n  \
- \ \___x___/  \ \_\ \_\ \__/.\_|\ \__\    \ \__\| \_\ \_\ \____\    \ \___,_\ \____/\ \____ \   \ \___,_\ \____/\ \_\ \_\ \_\       \n  \
-  \/__//__/    \/_/\/_/\/__/\/_/ \/__/     \/__/ \/_/\/_/\/____/     \/__,_ /\/___/  \/___L\ \   \/__,_ /\/___/  \/_/\/_/\/_/       \n  \
-                                                                                       /\____/                                      \n  \
-                                                                                       \_/__/                                       \n  \
-                            ┌─┐  ┌┬┐┌─┐┌─┐  ┌─┐┬┌┐┌┌┬┐┬┌┐┌┌─┐  ┌─┐┌┬┐┬  ┬┌─┐┌┐┌┌┬┐┬ ┬┬─┐┌─┐     \n  \
-                            ├─┤   │││ ││ ┬  ├┤ ││││ │││││││ ┬  ├─┤ ││└┐┌┘├┤ │││ │ │ │├┬┘├┤      \n  \
-                            ┴ ┴  ─┴┘└─┘└─┘  └  ┴┘└┘─┴┘┴┘└┘└─┘  ┴ ┴─┴┘ └┘ └─┘┘└┘ ┴ └─┘┴└─└─┘     \n  \
+def titleScreen():
+  print(" \
+                                                                _____^_       \n  \
+                                                              |    |  \       \n  \
+                                                              \   /  ^ |      \n  \
+                                                             / \_/   0  \     \n  \
+                                                            /            \    \n  \
+                                                           /    ____      0   \n  \
+                                                          /      /  \___ _/   \n  \
+               __                __        __    __                      __                          __                       __      \n  \
+             /\ \              /\ \__    /\ \__/\ \                    /\ \                        /\ \          __         /\ \      \n  \
+   __  __  __\ \ \___      __  \ \ ,_\   \ \ ,_\ \ \___      __        \_\ \    ___      __        \_\ \    ___ /\_\    ___ \ \/      \n  \
+  /\ \/\ \/\ |\ \  _ `\  /'__`\ \ \ \/    \ \ \/\ \  _ `\  /'__`\      /'_` \  / __`\  /'_ `\      /'_` \  / __`\/\ \ /' _ `\|/       \n  \
+  \ \ \_/ \_/| \ \ \ \/\ \L\.\_\ \ \_      \ \ \_\ \ \ \ \/\  __/     /\ \L\ \/\ \L\ \/\ \L\ \    /\ \L\ \/\ \L\ \ \ \/\ \/\ \        \n  \
+   \ \___x___/  \ \_\ \_\ \__/.\_|\ \__\    \ \__\| \_\ \_\ \____\    \ \___,_\ \____/\ \____ \   \ \___,_\ \____/\ \_\ \_\ \_\       \n  \
+    \/__//__/    \/_/\/_/\/__/\/_/ \/__/     \/__/ \/_/\/_/\/____/     \/__,_ /\/___/  \/___L\ \   \/__,_ /\/___/  \/_/\/_/\/_/       \n  \
+                                                                                         /\____/                                      \n  \
+                                                                                         \_/__/                                       \n  \
+                              ┌─┐  ┌┬┐┌─┐┌─┐  ┌─┐┬┌┐┌┌┬┐┬┌┐┌┌─┐  ┌─┐┌┬┐┬  ┬┌─┐┌┐┌┌┬┐┬ ┬┬─┐┌─┐     \n  \
+                              ├─┤   │││ ││ ┬  ├┤ ││││ │││││││ ┬  ├─┤ ││└┐┌┘├┤ │││ │ │ │├┬┘├┤      \n  \
+                              ┴ ┴  ─┴┘└─┘└─┘  └  ┴┘└┘─┴┘┴┘└┘└─┘  ┴ ┴─┴┘ └┘ └─┘┘└┘ ┴ └─┘┴└─└─┘     \n  \
 ")
+
+
+titleScreen()
 clearScreen(0.5)
 
 #ask which room player wants to go to
@@ -74,23 +85,17 @@ Where will you look for your dog?\n\
 
 #input question is in the print_slow above. cleanup is done in the same line
 room = input("").lower().replace("the","").replace(" ","")
+
 #clear screen and little bit of text before next question
 clearScreen(0.2)
 print_slow("You walk towards the ")
 print_slow(room)
 clearScreen(0.5)
 
-#lists for the 3 items player needs to pick up
-invList = ["item0","item1","item2"]
-kitchenList = ["·dog food", "·a knife", "·a spoon"]
-bathList = ["·soap", "·a towel", "·some toiletpaper"]
-bedList = ["·your pillow", "·your bed sheets", "·your sleeping girlfriend"]
-
 #var placeholders for listPrint
 kitchenCount = 0
 bathCount = 0
 bedCount = 0
-
 
 def roomsItems(itemNum): 
   #if statement for rooms and items
@@ -226,31 +231,34 @@ dogBedCorrectOrder3 = False
 #doBedOrder list
 #empty lists are seen as false
 #if any items in invList dog bed building sequence starts
-if invList:
-  for i in range(len(invList)):
-    if i == 0:
-      print_slow("Now that you have collected your items.\nYou start prepairing a dog bed in the hopes that your dog will return.\nWhich item do you put down first?\n")
-    elif i >= 1:
-      print_slow("Which item do you put down next?\n")
-    #inv print prints all items in inv with ·. ergo not using listPrint
-    invPrint()
-    dogBedOrder = input('').lower()
-    #item 1 should be towel
-    if (dogBedOrder == "towel" or dogBedOrder == "bed sheets") and i == 0:
-      dogBedCorrectOrder1 = True
-      invList.remove(dogBedOrder)
-    #item 2 should be pillow
-    elif dogBedOrder == "pillow" and i == 1:
-      dogBedCorrectOrder2 = True
-      invList.remove(dogBedOrder)
-    elif dogBedOrder == "dog food" and i == 2:
-      dogBedCorrectOrder3 = True
-      invList.remove(dogBedOrder)
+def invListing():
+  if invList:
+    for i in range(len(invList)):
+      if i == 0:
+        print_slow("Now that you have collected your items.\nYou start prepairing a dog bed in the hopes that your dog will return.\nWhich item do you put down first?\n")
+      elif i >= 1:
+        print_slow("Which item do you put down next?\n")
+      #inv print prints all items in inv with ·. ergo not using listPrint
+      invPrint()
+      dogBedOrder = input('').lower()
+      #item 1 should be towel
+      if (dogBedOrder == "towel" or dogBedOrder == "bed sheets") and i == 0:
+        dogBedCorrectOrder1 = True
+        invList.remove(dogBedOrder)
+      #item 2 should be pillow
+      elif dogBedOrder == "pillow" and i == 1:
+        dogBedCorrectOrder2 = True
+        invList.remove(dogBedOrder)
+      elif dogBedOrder == "dog food" and i == 2:
+        dogBedCorrectOrder3 = True
+        invList.remove(dogBedOrder)
+      else:
+        invList.remove(dogBedOrder)
+      clearScreen(1.5)
     else:
-      invList.remove(dogBedOrder)
-    clearScreen(1.5)
-else:
-  pass
+      pass
+invListing()
+
 
 if dogBedCorrectOrder1 == True and dogBedCorrectOrder2 == True and dogBedCorrectOrder3 == True:
   print_slow('You hear your dog come home through their doggy door.\nAs they lay down on the doggy bed you made you ask your dog a question.\n"so what were you doing?"')
@@ -258,10 +266,11 @@ elif emptyHanded == False:
   print_slow("You sit down on the couch waiting for your dog to come in but they don't come. ")
   print_slow('"Eh they');print_slow("'ll be fine");print_slow('" you say to yourself als you fall asleep on the couch.')
 
-time.sleep(2)
-print("\n \
-  ______         __            ______            __  __                     __ \n\
- /_  __/___     / /_  ___     / ____/___  ____  / /_/_/___  __  _____  ____/ /\n\
-  / / / __ \   / __ \/ _ \   / /   / __ \/ __ \/ __/ / __ \/ / / / _ \/ __  / \n\
- / / / /_/ /  / /_/ /  __/  / /___/ /_/ / / / / /_/ / / / / /_/ /  __/ /_/ /  \n\
-/_/  \____/  /_.___/\___/   \____/\____/_/ /_/\__/_/_/ /_/\__,_/\___/\__,_/   \n")
+def continued():
+  time.sleep(2)
+  print("\n \
+    ______         __            ______            __  __                     __ \n\
+   /_  __/___     / /_  ___     / ____/___  ____  / /_/_/___  __  _____  ____/ /\n\
+    / / / __ \   / __ \/ _ \   / /   / __ \/ __ \/ __/ / __ \/ / / / _ \/ __  / \n\
+   / / / /_/ /  / /_/ /  __/  / /___/ /_/ / / / / /_/ / / / / /_/ /  __/ /_/ /  \n\
+  /_/  \____/  /_.___/\___/   \____/\____/_/ /_/\__/_/_/ /_/\__,_/\___/\__,_/   \n")
